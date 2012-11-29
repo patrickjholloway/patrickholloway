3f55195c6033d5f79c8b418068a5b5e6c9a89129
Patrick Holloway <patrickjholloway@gmail.com>
2012-11-29 01:09:34 -0500

    front page constructed with empty links

---
+++ vendor/assets/javascripts/rails.validations.customValidators.js
@@ -0,0 +1,18 @@
+window.ClientSideValidations.validators.local['email'] = function(element, options) {
+  // Your validator code goes in here
+  if (!/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test(element.val())) {
+    // When the value fails to pass validation you need to return the error message.
+    // It can be derived from validator.message
+    return options.message;
+  }
+}
+
+window.ClientSideValidations.validators.local['dropkick'] = function(element, options) {
+  // Your validator code goes in here
+  if (!/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test(element.val())) {
+    // When the value fails to pass validation you need to return the error message.
+    // It can be derived from validator.message
+    return options.message;
+  }
+}
+
