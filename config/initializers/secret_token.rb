abdaba6959fc38f0d708c82ed9881244ebf3fcb3
Patrick Holloway <patrickjholloway@gmail.com>
2012-11-28 19:49:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patrickholloway::Application.config.secret_token = '971d22bf365324fb8fde6b28bf849b8a418542102e1ba8cc093ce5ff0f2529063ba3d7bc0bc394db42744c4b2e14242bfe9d54360fa9562005fc62320fa36878'
